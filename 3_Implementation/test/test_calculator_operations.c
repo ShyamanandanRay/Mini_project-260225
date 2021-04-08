5310b6c95be6d6e4de4e2bdb2fd34a3e4516333c
shyama <shyamanandan.ray7381@gmail.com>
2021-04-08 23:44:53 +0530

    Enter

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -7,6 +7,7 @@
 
 /* Prototypes for all the test functions */
 void test_add(void);
+void test_add_testcase2(void);
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
@@ -24,6 +25,7 @@ int main()
 
 /* Run Test functions */
   RUN_TEST(test_add);
+  RUN_TEST(test_add_testcase2);
   RUN_TEST(test_subtract);
   RUN_TEST(test_multiply);
   RUN_TEST(test_divide);
@@ -35,28 +37,32 @@ int main()
 /* Write all the test functions */ 
 void test_add(void) {
   TEST_ASSERT_EQUAL(30, add(10, 20));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(15000, add(7500, 7500));
+  TEST_ASSERT_EQUAL(-10, add(10, -20));
+  TEST_ASSERT_EQUAL(-30, add(-10, -20));
+  TEST_ASSERT_EQUAL(10, add(-10, 20));
 }
-
+void test_add_testcase2(void) {
+ 
+  /* Dummy fail*/
+  // TEST_ASSERT_EQUAL(1500, add(750, 7500));
+}
 void test_subtract(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(100, subtract(1000, 900));
+  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
 }
 
 void test_multiply(void) {
   TEST_ASSERT_EQUAL(0, multiply(1, 0));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(10, multiply(2, 5));
+  // TEST_ASSERT_EQUAL(2, multiply(2, 5));
 }
 
 void test_divide(void) {
   TEST_ASSERT_EQUAL(0, divide(1, 0));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, divide(2, 2));
+  // TEST_ASSERT_EQUAL(3, divide(2, 2));
 }
