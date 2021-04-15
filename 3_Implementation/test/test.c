ea3c64c79ccaadd24931d1ce594ddf7e2f34ce86
shyama <shyamanandan.ray7381@gmail.com>
2021-04-15 13:20:37 +0530

    enter

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -9,8 +9,21 @@ void test_login(void)
     TEST_ASSERT_EQUAL(1,view()>0);
 }
 
+void test_insert(void)
+{
+    TEST_ASSERT_EQUAL(1,insert()>0);
+}
+
+void test_old(void)
+{
+    TEST_ASSERT_EQUAL(1,old()>0);
+}
+
 int test_main(void)
 {
+    UNITY_BEGIN();
     RUN_TEST(test_login);
+    RUN_TEST(test_insert);
+    RUN_TEST(test_old);
     return UNITY_END();
 }
