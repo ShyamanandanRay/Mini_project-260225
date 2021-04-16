b3ff5a4ff030d2c6a7ba7a528c8378cf4640f774
shyama <shyamanandan.ray7381@gmail.com>
2021-04-16 11:11:12 +0530

    enter

---
+++ 3_Implementation/src/change_price.c
@@ -0,0 +1,17 @@
+#include "header.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int change_price(int prize)
+{
+	
+	
+		printf("Confirm the price: ");
+		scanf("%d",&prize);
+		system("PAUSE");
+		system("CLS");
+	
+	
+	return prize;
+}
